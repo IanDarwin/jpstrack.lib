fea64317f506d4185115933cea328f841090ed55
IanDarwin <>
2013-04-03 22:59:50 +0000

    Code to upload one GPX track. Will merge into jpstrack core at some point soon.

---
+++ src/jpstrack/upload/TrackVisibility.java
@@ -0,0 +1,8 @@
+package jpstrack.upload;
+
+/** The visibility the track(s) shall have on OSM, as explained at http://www.openstreetmap.org/traces/mine
+ * @author Ian Darwin
+ */
+public enum TrackVisibility {
+	PRIVATE, PUBLIC, TRACKABLE, IDENTIFIABLE
+}
---
+++ src/jpstrack/upload/Upload.java
@@ -0,0 +1,93 @@
+package jpstrack.upload;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.darwinsys.io.FileIO;
+
+
+public class Upload {
+
+	private static final String FILENAME = "testdata.gpx";
+	private final static String BOUNDARY = "OSM_TRACK_FILE_42_GNORNMPLATZ";
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		String description = "This is a test track";
+		TrackVisibility visibility = TrackVisibility.IDENTIFIABLE;
+		File gpxFile = new File(FILENAME);
+		if (!gpxFile.canRead()) {
+			throw new IOException("Can't read file " + FILENAME);
+		}
+		String response = converse("api06.dev.openstreetmap.org", 80, "/api/0.6/gpx/create",
+				"XXX", "YYY",
+				encodePostBody( description, visibility, gpxFile));
+
+		System.out.println("Server responded thus: " + response);
+	}
+
+	/** Handle the HTTP POST conversation */
+	public static String converse(String host, int port, String path, String userName, String password, String postBody) throws IOException {
+		URL url = new URL("http", host, port, path);
+		URLConnection conn = url.openConnection();
+		conn.setRequestProperty("Accept", "application/xml");
+		conn.setRequestProperty("Authorization", "Basic " + Base64.encodeBytes((userName + ":" + password).getBytes()));
+		conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+		conn.setDoInput(true);	// we want POST!
+		conn.setDoOutput(true);
+		conn.setAllowUserInteraction(true);
+
+		conn.connect();
+
+		final OutputStream outputStream = conn.getOutputStream();
+		outputStream.write(postBody.getBytes());
+		outputStream.close();
+
+		StringBuilder sb = new StringBuilder();
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+						conn.getInputStream()));
+		String line;
+		while ((line = in.readLine()) != null) {
+			sb.append(line);
+		}
+		in.close();
+		return sb.toString();
+	}
+
+	/** Get the GPX file and other parameters into shape for POSTing */
+	public static String encodePostBody(String description, TrackVisibility visibility, File gpxFile) throws IOException {
+		StringBuffer body = new StringBuffer();
+		body.append(encodePlainTextPart("description", description, true));
+		body.append(encodePlainTextPart("visibility", visibility.toString().toLowerCase(), false));
+
+		body.append("\r\n--" + BOUNDARY + "\r\n");
+		body.append("Content-Disposition: form-data; name=\"" + gpxFile.getName() + "\"\r\n");
+		body.append("Content-Type: application/xml\r\n");
+		body.append("Content-Transfer-Encoding: text\r\n\r\n");
+
+		body.append(FileIO.readerToString(new FileReader(gpxFile)));
+
+		body.append("\r\n--" + BOUNDARY + "--");
+		return body.toString();
+	}
+
+	private static String encodePlainTextPart(String partName, String partBody, boolean first) {
+		return (first? "" : "\r\n") + "--" + BOUNDARY + "\r\n"
+				+ "Content-Disposition: form-data; name=\"" + partName + "\"\r\n\r\n" 
+				+ partBody;
+	}
+
+
+}
