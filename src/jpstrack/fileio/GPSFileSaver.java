901a01c5a10b9a874c8cb43fb6ee942a8e724711
Ian Darwin <ian@darwinsys.com>
2014-04-14 21:58:01 -0400

    Clean up file format a bit, now acceptable to OSM importer.

--- src/jpstrack/fileio/GPSFileSaver.java
+++ src/jpstrack/fileio/GPSFileSaver.java
@@ -38,23 +38,23 @@ public class GPSFileSaver {
 		File f = new File(startingDir, fileName);
 		try {
 			out = new PrintWriter(f);
-			out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-			out.println("<gpx version=\"1.1\" creator=\"GpsTrack java client\"");
-			out.println("	xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
-			out.println("	xmlns='http://www.topografix.com/GPX/1/1'");
-			out.println("	xsi:schemaLocation='http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd'>");
+			out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>");
+			out.println("<gpx version=\"1.1\"");
+			out.println("    creator=\"GpsTrack java client\"");
+			out.println("    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
+			out.println("    xmlns='http://www.topografix.com/GPX/1/1'");
+			out.println("    xsi:schemaLocation='http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd'>");
 			out.println("  <metadata>");
-			out.println("  <name>JpsTrack Java Client</name>");
-			out.println("    <link href=\"http://www.darwinsys.com/jpstrack/\">");
-			out.println("      <text>Ian Darwin - jpstrack</text>");
-			out.println("    </link>");
-			out.println("    <author>Ian Darwin, Rejminet Group Inc.</author>");
+			out.println("    <name>jpstrack GPS Receiver track log</name>");
+			out.println("    <time>" + df.format(new Date()) + "</time>");
 			out.println("  </metadata>");
 
 			out.println("");
 
 			out.println("<!-- track start -->");
 			out.println("<trk>");
+			out.println("<src>Logged using jpstrack</src>");
+			out.println("<link href=\"http://darwinsys.com/jpstrack\"><text>RejmiNet Group Inc.</text></link>");
 			out.println("<trkseg>");
 			return f;
 		} catch (FileNotFoundException e) {
@@ -62,15 +62,15 @@ public class GPSFileSaver {
 		}
 	}
 	DateFormat df = 
-		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.00Z");
 	Date date = new Date();
 	
 	public void write(long time, double latitude, double longitude) {
 		out.printf("<trkpt lat='%f' lon='%f'>%n", latitude, longitude);
-		out.printf("    <fix>3d</fix>%n");
 		out.printf("    <ele>0</ele>%n");
 		date.setTime(time);
 		out.printf("    <time>%s</time>%n", df.format(date));
+		out.printf("    <fix>3d</fix>%n");
 		out.printf("</trkpt>%n");
 		out.flush();
 	}
@@ -80,7 +80,7 @@ public class GPSFileSaver {
 	}
 	
 	/** Close the file, after outputting the trailing end tags */
-	public void endFile() {
+	public void close() {
 		out.println("</trkseg>");
 		out.println("</trk>");
 		out.println("</gpx>");
@@ -88,9 +88,9 @@ public class GPSFileSaver {
 		out.close();
 	}
 	
-	/** Alternate name for close functionality */
-	public void close() {
-		endFile();
+	/** Historical name for close functionality */
+	public void endFile() {
+		close();
 	}
 	
 	public File getStartingDir() {
