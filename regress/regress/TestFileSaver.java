e316c91707f5b4811d52c8aeef77242926ba7179
Ian Darwin <ian@darwinsys.com>
2014-04-14 21:59:48 -0400

    Generate a series of data from an array, not just one point.

--- regress/regress/TestFileSaver.java
+++ regress/regress/TestFileSaver.java
@@ -21,22 +21,31 @@ public class TestFileSaver {
 	public void setUp() throws Exception {
 		saver = new GPSFileSaver();
 	}
+	
+	double[][] data = {
+		{45.678900, -80.000000, 1397526902100L },
+		{44.068195, -79.506640, 1397526903200L },
+		{44.068249, -79.506356, 1397526904300L },
+		{44.068249, -79.506190, 1397526905400L },
+	};
 
 	@Test
 	public final void testWrite() throws IOException {
 		final File dirFile = new File(System.getProperty("java.tmp.dir", "/tmp"));
 		saver.setStartingDir(dirFile);
-		saver.setFileName("testwrite.gpx");
+		saver.setFileName("testdata.gpx");
 		saver.startFile();
-		Reading r = new Reading();
-		r.setLatitude(23.4567d);
-		r.setLongitude(45.6789d);
-		r.setTime(1000000000L);
-		saver.write(r);
+		for (double[] row : data) {
+			Reading r = new Reading();
+			r.setLatitude(row[0]);
+			r.setLongitude(row[1]);
+			r.setTime((long)row[2]);
+			saver.write(r);
+		}
 		saver.close();
 		String str = FileIO.readAsString(
 				new File(dirFile, saver.getFileName()).getAbsolutePath());
-		assertTrue(str.indexOf("23.4567") > 0);
+		assertTrue(str.indexOf("45.67890") > 0);
 		// ensure file written (and read) to ends
 		assertTrue(str.indexOf("</gpx>") > 0);
 	}
